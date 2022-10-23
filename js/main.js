163ebd8b79e664b3d380eb787f3952a53d21f3d9
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-23 17:30:38 +0200

    working on buttons and information about current image

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ function removeClass () {
         el.classList.remove('currentActive')
     })
 }
+
+
+const roundNumbers = document.querySelectorAll('.roundNumbers');
+const buttons = document.querySelectorAll('.button')
+buttons.forEach((el) => {el.addEventListener('click', changeColor)})
+
+function changeColor() {
+    for( let i = 0; i < 5; i++){
+        if(images[i].classList[2] == 'active'){
+            
+            roundNumbers[i].classList.add('activeRoundButton')
+        }
+    }
+        
+}
