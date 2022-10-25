477876829bb94b0bc44d9b0f5c340813b9cd72fc
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-25 08:27:18 +0200

    working bar and animations

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
+const roundNumbers = document.querySelectorAll('.roundNumbers');
 const images = document.querySelectorAll('.image__container');
+const next = document.querySelector('.buttonNext')
+const prev = document.querySelector('.buttonPrev')
+const bars = document.querySelectorAll('.bar')
 
-images.forEach((el) => el.addEventListener('click', () => {
-    removeClass()
-    el.classList.add('active')
-    changeColor()
-}))
 
-function removeClass () {
-    images.forEach((el) => {
-        el.classList.remove('active')
-        el.classList.remove('currentActive')
-    })
+next.addEventListener('click', fillNextNumber)
+prev.addEventListener('click', washPrevNumber)
+
+let currentActive = 0
+let currentBar = 0
+
+ async function fillNextNumber() {
+    if(currentActive < 4 || currentBar <= 3){
+        currentActive++
+        roundNumbers[currentActive].classList.add('activeRoundButton')
+        bars[currentBar].classList.add('activeConnection')
+        currentBar++
+        showNextImage()
+    } else {
+        currentActive = 4
+        currentBar = 4
+    }  
 }
 
+ async function washPrevNumber() {
+    if(currentActive > 0 || currentBar > 0){
+        roundNumbers[currentActive].classList.remove('activeRoundButton')
+        currentActive--
+        currentBar--
+        bars[currentBar].classList.remove('activeConnection')
+        showPrevImage()
+    } else {
+        currentActive = 0
+        currentBar = 0
+     }
+}
 
-const roundNumbers = document.querySelectorAll('.roundNumbers');
-const buttons = document.querySelectorAll('.button')
-// buttons.forEach((el) => {el.addEventListener('click', changeColor)})
-
-function changeColor() {
-    for( let i = 0; i < 5; i++){
-        if(images[i].classList[2] == 'active'){
-            
-            roundNumbers[i].classList.add('activeRoundButton')
-        }
+function showNextImage() {
+    if(currentActive >= 0){
+        images[0].classList.remove('currentActive')
+        images[currentActive-1].classList.remove('active')
+        images[currentActive].classList.add('active')
+        console.log(currentActive)
     }
-        
+}
+
+function showPrevImage() {
+    images[currentActive].classList.add('active')
+    images[currentActive +1].classList.remove('active')
 }
